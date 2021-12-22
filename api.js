bd2749a13ad4f3dd0ddc03b441bf611cc0c9a88c
Monique McIntyre <skaiwilliams85@gmail.com>
2021-12-22 15:42:37 -0500

    removed files

--- api.js
+++ api.js
@@ -1,2 +1,26 @@
 require("dotenv").config();
-console.log(process.env);
+let clientID = process.env.CLIENT_ID;
+let clientSECRET = process.env.CLIENT_SECRET;
+
+//console.log(process.env);
+
+//Selectors
+const form = document.querySelector("form");
+
+//get input
+let getInput = (e) => {
+  e.preventDefault();
+  let userInput = form.username.value.trim();
+  console.log(userInput);
+  return userInput;
+};
+
+//Fetch data from API
+let getData = async (userInput) => {
+  url = `https://api.github.com/users${userInput}?client_id=${clientID}&client_secret=${clientSECRET}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+};
+//Event Listener
+form.addEventListener("submit", getInput);
